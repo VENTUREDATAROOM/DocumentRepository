5cd711c92860c394b81b54ea702065692463c7d8
hp <hp@DESKTOP-P9AVAHE>
2023-11-25 18:08:02 +0530

    learn 1

--- src/main/java/com/document/contoller/DriverDetailController.java
+++ src/main/java/com/document/contoller/DriverDetailController.java
@@ -18,6 +18,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping("/api")
 @Tag(name = "Dashboardrelated API")
+
+
 public class DriverDetailController {
 
 	@Autowired
